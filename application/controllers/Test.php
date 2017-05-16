b7027e6204369d568339c8805bd79cc374f709b8
David Santos <insa1988@gmail.com>
2017-05-16 16:31:50 -0500

    creacion controlador de prueba

---
+++ application/controllers/Test.php
@@ -0,0 +1,11 @@
+<?php 
+   class Test extends CI_Controller {
+  
+      public function index() { 
+         echo "Hello World!"; 
+      }
+      public function hellow(){
+        echo "test en hellow";
+      }
+   } 
+?>
