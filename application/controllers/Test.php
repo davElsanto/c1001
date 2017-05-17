de222b9ef65e1a6dbf4098d836e725aa620ecc33
David Santos <insa1988@gmail.com>
2017-05-16 20:03:12 -0500

    agrego insert a db automatica y manual

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -11,5 +11,23 @@
       public function viewtest(){
         $this->load->view('viewtest');
       }
+
+      public function insertdata(){
+        $data = array(
+          'roll_no' => '1',
+          'name' => 'test1'
+        );
+        $this->db->insert('stud', $data);
+      }
+
+      public function insertdata2(){
+        $this->load->database();
+         $data = array(
+          'roll_no' => '2',
+          'name' => 'test2'
+        );
+        echo $this->db->insert('stud', $data);
+        echo $this->db->close();
+      }
    } 
 ?>
